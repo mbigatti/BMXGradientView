35d679c9f1c16d093ef6a01e48777e262f7771fc
mbigatti <massimiliano@bigatti.it>
2014-07-21 13:29:24 +0200

    Initial Commit

---
+++ BMXGradientView/BMXGradientView.h
@@ -0,0 +1,19 @@
+//
+//  BMXGradientView.h
+//  BMXGradientView
+//
+//  Created by Massimiliano Bigatti on 21/07/14.
+//  Copyright (c) 2014 Massimiliano Bigatti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BMXGradientView.
+FOUNDATION_EXPORT double BMXGradientViewVersionNumber;
+
+//! Project version string for BMXGradientView.
+FOUNDATION_EXPORT const unsigned char BMXGradientViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BMXGradientView/PublicHeader.h>
+
+
